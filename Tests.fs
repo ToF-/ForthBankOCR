18c3510e19fa73999fee5a1019fb8fa460761c04
ToF- <cthibaut@octo.com>
2016-02-19 14:51:32 +0100

    commenting tests

--- Tests.fs
+++ Tests.fs
@@ -33,19 +33,22 @@ TEST      30 TYPE CR
 TEST 30 + 30 TYPE CR
 TEST 60 + 30 TYPE CR
 
-CREATE ILL-TEST
- _  ...  _   _  ...  _   _  _   _   _
-|.|   |  _|  _| |_| |_  |_   | |_| |_|
-|_|   | |_   _|   |  _| |_|  | |_|  _|
-
 
 10 ACCOUNT-SIZE !
 
 : AUTOTEST
+    \ detecting a vertical or horizontal bar
     ASSERT( BL   BAR? 0 = )
     ASSERT( PIPE BAR? 1 = )
     ASSERT( BAR  BAR? 1 = )
 
+    \ position of an OCR bar (10 x 3 char digits per line) : 
+    \     0123
+    \    ..... 
+    \  0 . 0  
+    \ 30 .123
+    \ 60 .456
+    
     ASSERT( 0 0  1 CHECK-ADDRESS )
     ASSERT( 0 1 30 CHECK-ADDRESS )
     ASSERT( 0 2 31 CHECK-ADDRESS )
@@ -58,19 +61,23 @@ CREATE ILL-TEST
     ASSERT( 1 2 34 CHECK-ADDRESS )
     ASSERT( 9 6 89 CHECK-ADDRESS )
 
-
     TEST OCR 90 CMOVE
+    \ converts OCR digits into patterns
     ASSERT( 0 [ 2 BASE ! ] OCR>PATTERN 1101111 = [ DECIMAL ] )
     ASSERT( 1 [ 2 BASE ! ] OCR>PATTERN 0001001 = [ DECIMAL ] )
     ASSERT( 5 [ 2 BASE ! ] OCR>PATTERN 1110011 = [ DECIMAL ] )
+    ASSERT( 9 [ 2 BASE ! ] OCR>PATTERN 1111011 = [ DECIMAL ] )
 
     ASSERT( [ 2 BASE ! ] 1111111 [ DECIMAL ] PATTERN>DIGIT [CHAR] 8 = )
     
+    \ converts OCR digits into account number
     OCR>ACCOUNT .ACCOUNT SPACE
 
-    ILL-TEST OCR 90 CMOVE
-    ASSERT( BL 0 0 ADDRESS C! OCR>ACCOUNT ACCOUNT C@ [CHAR] ? = ) 
+    \ a missing bar in one digit makes the account illegible
+    ASSERT( BL 0 0 ADDRESS C!  
+            OCR>ACCOUNT ACCOUNT C@ [CHAR] ? = ) 
     ASSERT( ILLEGIBLE? )
+    
     BL 1 3 ADDRESS C!
     
     OCR>ACCOUNT .ACCOUNT SPACE
@@ -80,7 +87,6 @@ CREATE ILL-TEST
     ASSERT( ERROR? 0= )
     S" 457508001" ACCOUNT SWAP CMOVE
     ASSERT( ERROR? )
-    .ACCOUNT SPACE
 
 
     ." TESTS PASSED"
