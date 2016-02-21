ee4f8751632a27ab46fc801254f2c6df128c98f6
ToF- <cthibaut@octo.com>
2016-02-21 19:38:23 +0100

    adding a bit to an OCR

--- BankOcr.fs
+++ BankOcr.fs
@@ -1,25 +1,47 @@
+\ BankOcr 
+3 CONSTANT WIDTH
+9 CONSTANT SIZE
+2 BASE !
+CHAR ? CONSTANT NOT-FOUND
+CREATE DIGITS  ( OCR patterns in digit order 0,1,2..9 )
+010101111 C, 000001001 C, 010011110 C, 010011011 C, 000111001 C,  
+010110011 C, 010110111 C, 010001001 C, 010111111 C, 010111011 C,
+: >CHAR 110000 OR ;
+: >INT  001111 AND ;
+DECIMAL 
 : BAR?   32 <> 1 AND ;
 : BIT<<  SWAP 1 LSHIFT OR ;  
 
-: OCR>BITS ( src u dst -- )
+: OCR>BITS ( src,u,dst -- converts an OCR line into bits )
     SWAP 0 DO 2DUP        
-        I 3 / + DUP C@     
+        I WIDTH / + DUP C@     
         ROT I + C@ BAR? 
         BIT<< SWAP C! 
     LOOP 2DROP ;
 
+: BIT-OCR>DIGIT ( byte -- c  find the digit corresponding to the ocr bit pattern or '?' )
+    NOT-FOUND 10 0 DO 
+        OVER DIGITS I + C@ = IF DROP I LEAVE THEN 
+    LOOP NIP >CHAR ;
 
-2 BASE !
-CREATE DIGITS  
-010101111 C, 000001001 C, 010011110 C, 010011011 C, 000111001 C,  
-010110011 C, 010110111 C, 010001001 C, 010111111 C, 010111011 C,
-: >CHAR 110000 OR ;
-DECIMAL 
+: >DIGITS ( src,dst,n -- converts OCR bit patterns into digits )
+    0 DO 
+        OVER I + C@ BIT-OCR>DIGIT 
+        OVER I + C!
+    LOOP 2DROP ;
 
-CHAR ? CONSTANT NOT-FOUND
-: >DIGIT NOT-FOUND 10 0 DO OVER DIGITS I + C@ = IF DROP I LEAVE THEN LOOP NIP >CHAR ;
+: LEGIBLE ( src,n -- f|t true is the string doesn't contain ? )
+    FALSE SWAP 0 DO 
+        OVER I + C@ NOT-FOUND = OR 
+    LOOP 0= NIP ;
 
-: >DIGITS 0 DO DUP I + C@ >DIGIT OVER I + C! LOOP DROP ;
+: CHECKSUM ( src,n -- f|t true if the account checksums )
+    0 SWAP 0 DO
+        OVER I + C@ >INT SIZE I - * + 
+    LOOP 11 MOD 0= NIP ;
 
+: ADD-OCR-BIT ( adr n b -- adds a bit in position b on adr+n )
+    1 SWAP LSHIFT -ROT + DUP C@ ROT OR SWAP C! ; 
+        
 
 
--- Tests.fs
+++ Tests.fs
@@ -2,6 +2,7 @@
 
 S" BankOcr.fs" REQUIRED
 
+CREATE ACCOUNT 10 ALLOT
 
 
 : AUTOTEST
@@ -21,14 +22,23 @@ S" |_|  ||_  _|  | _||_|  ||_| _|" PAD OCR>BITs
     [ 2 BASE ! ]
     ASSERT( PAD 1001 + C@ 10111011 = )
     [ DECIMAL ]
-    PAD 10 >DIGITS
-    ASSERT( S" 0123456789" PAD 10 COMPARE 0= )
+    PAD ACCOUNT 10 >DIGITS
+    ASSERT( S" 0123456789" ACCOUNT 10 COMPARE 0= )
     PAD 10 ERASE
 S"  _     _  _     _  _  _     _ " PAD OCR>BITs
 S" | |  | _| _|| ||_ |_   ||_||_|" PAD OCR>BITs
 S" |_|  ||_  _|  | _||_|  ||_| _|" PAD OCR>BITs
-    PAD 10 >DIGITS
-    ASSERT( S" 0123?567?9" PAD 10 COMPARE 0= )
+    PAD ACCOUNT 10 >DIGITS
+    ASSERT( S" 0123?567?9" ACCOUNT 10 COMPARE 0= )
+    ASSERT( S" 0123456789" LEGIBLE ) 
+    ASSERT( S" 012345?789" LEGIBLE 0= ) 
+    ASSERT( S" 457508000"  CHECKSUM )   
+    ASSERT( S" 457518000"  CHECKSUM 0= )   
+    PAD 4 4 ADD-OCR-BIT
+    PAD 8 7 ADD-OCR-BIT
+    PAD ACCOUNT 10 >DIGITS
+    ASSERT( S" 0123456789" ACCOUNT 10 COMPARE 0= )
+    
 
 
   ." TESTS PASSED" 
