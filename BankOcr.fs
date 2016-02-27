9970c2014cf892e75064b8c172d31d8d0089784a
ToF- <cthibaut@octo.com>
2016-02-27 18:35:05 +0100

    ENCODE-LINE

--- BankOcr.fs
+++ BankOcr.fs
@@ -11,3 +11,10 @@
         DUP I + C@ BAR?
         ROT BIT! SWAP
     LOOP DROP ;
+
+: ENCODE-LINE ( dst,str,n -- )
+    3 / 0 DO            
+        OVER C@ OVER 3 ENCODE   
+        >R OVER R> SWAP C!
+        SWAP 1+ SWAP 3 + 
+    LOOP 2DROP ;        
--- Tests.fs
+++ Tests.fs
@@ -20,10 +20,21 @@ S" BankOcr.fs" REQUIRED
             10101111 =?
             [ DECIMAL ] ) ;
                 
+: TEST_ENCODING_OCR_LINE 
+    PAD 2 ERASE
+    PAD S"  _    " ENCODE-LINE
+    PAD S" | |  |" ENCODE-LINE
+    PAD S" |_|  |" ENCODE-LINE
+    [ 2 BASE ! ]
+    ASSERT( PAD 0 + C@  10101111 =? )
+    ASSERT( PAD 1 + C@  00001001 =? )
+    [ DECIMAL ] ;
+
 : TESTS 
     TEST_ENCODING_OCR
+    TEST_ENCODING_OCR_LINE
 ;
-
+PAGE
 TESTS
 ." SUCCESS"
 BYE
